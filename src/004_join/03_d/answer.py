6c2fc545216eb5d826526e4a0edd719094b85497
negi524 <egtnrumpbiizarre.info@gmail.com>
2021-09-29 08:48:35 +0900

    演習環境構築

---
+++ src/004_join/03_d/answer.py
@@ -0,0 +1,9 @@
+from preprocess.load_data.data_loader import load_hotel_reserve
+import pandas as pd
+customer_tb, hotel_tb, reserve_tb = load_hotel_reserve()
+
+def main():
+    print('hello')
+
+if __name__ == '__main__':
+    main()
---
+++ src/README.md
@@ -0,0 +1,14 @@
+
+# 実行方法
+
+仮想環境に入る
+
+```bash
+pipenv shell
+```
+
+モジュールとして実行する
+
+```bash
+python -m src.004_join.03_d.answer
+```
